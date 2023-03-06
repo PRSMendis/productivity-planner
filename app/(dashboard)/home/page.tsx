a540c3de60d82977446dd061aa6c1dcc40fe52ce
Rayhan Mendis <rayhan.mendis@gmail.com>
2023-03-07 10:25:44 +1100

    fixing cssd

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,4 +1,5 @@
 import Greetings from "@/components/Greetings";
+import GreetingsSkeleton from "@/components/GreetingsSkelteon";
 import { delay } from "@/lib/async"
 import { getUserFromCookie } from "@/lib/auth";
 import { db } from "@/lib/db";
@@ -8,12 +9,12 @@ import { Suspense } from "react";
 
 export default async function Page() {
   return (
-    <div className="h-full overflow-y-auto pr-6 w-1/1">
+    <div className="h-full overflow-y-auto pr-6 w-full">
       <div className=" h-full  items-stretch justify-center min-h-[content]">
         <div className="flex-1 grow flex">{/** greetings here */}</div>
         <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
           {/** projects map here */}
-          <Greetings/>
+          <Suspense fallback={<GreetingsSkeleton/>}><Greetings/></Suspense>
           <div className="w-1/3 p-3">{/* new project here */}</div>
         </div>
         <div className="mt-6 flex-2 grow w-full flex">
