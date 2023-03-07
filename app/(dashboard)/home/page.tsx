277212d21d21e016368f65ded20023bd669bc71f
Rayhan Mendis <rayhan.mendis@gmail.com>
2023-03-07 11:28:36 +1100

    fixing css

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -11,7 +11,6 @@ import { Suspense } from "react";
 const getData = async () => {
   // await delay(1000);
   const user = await getUserFromCookie(cookies());
-  console.log('user: ', user);
   const projects = await db.project.findMany({
     where: {
       ownerId: user.id,
@@ -20,24 +19,22 @@ const getData = async () => {
       tasks: true,
     },
   });
-
-  console.log('projects: ', projects);
   return { projects };
 };
 
 export default async function Page() {
 
   const {projects} = await getData();
-  // console.log('projects: ', projects);
 
   return (
     <div className="h-full overflow-y-auto pr-6 w-full">
       <div className=" h-full  items-stretch justify-center min-h-[content]">
-        <div className="flex-1 grow flex">{/** greetings here */}</div>
-        <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
+        <div className="flex-1 grow flex">
           <Suspense fallback={<GreetingsSkeleton/>}><Greetings/></Suspense>
+        </div>
+        <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
           {projects.map(project => (
-            <div>
+            <div className="w-1/3 p-3" key={project.id}>
               <Link href={`/project/${project.id}`}>
                 <ProjectCard project={project} />
               </Link>
