eb3bb3308c0c531af74842cce3817b8233af643a
Rayhan Mendis <rayhan.mendis@gmail.com>
2023-03-07 11:23:00 +1100

    succesfully hydrating project data

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,5 +1,6 @@
 import Greetings from "@/components/Greetings";
 import GreetingsSkeleton from "@/components/GreetingsSkelteon";
+import ProjectCard from "@/components/ProjectCards";
 import { delay } from "@/lib/async"
 import { getUserFromCookie } from "@/lib/auth";
 import { db } from "@/lib/db";
@@ -13,32 +14,35 @@ const getData = async () => {
   console.log('user: ', user);
   const projects = await db.project.findMany({
     where: {
-      ownerId: user?.id,
+      ownerId: user.id,
     },
     include: {
       tasks: true,
     },
   });
 
+  console.log('projects: ', projects);
   return { projects };
 };
 
 export default async function Page() {
 
-  const {projects} = getData();
-  console.log('projects: ', projects);
+  const {projects} = await getData();
+  // console.log('projects: ', projects);
 
   return (
     <div className="h-full overflow-y-auto pr-6 w-full">
       <div className=" h-full  items-stretch justify-center min-h-[content]">
         <div className="flex-1 grow flex">{/** greetings here */}</div>
         <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
+          <Suspense fallback={<GreetingsSkeleton/>}><Greetings/></Suspense>
           {projects.map(project => (
             <div>
-              <Link></Link>
+              <Link href={`/project/${project.id}`}>
+                <ProjectCard project={project} />
+              </Link>
             </div>
           ))}
-          <Suspense fallback={<GreetingsSkeleton/>}><Greetings/></Suspense>
           <div className="w-1/3 p-3">{/* new project here */}</div>
         </div>
         <div className="mt-6 flex-2 grow w-full flex">
