ad98b3ac21d476a36992daf4c2a21f7290561262
Rayhan Mendis <rayhan.mendis@gmail.com>
2023-03-07 11:10:37 +1100

    added some page cards

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -7,13 +7,36 @@ import { cookies } from "next/headers";
 import Link from "next/link";
 import { Suspense } from "react";
 
+const getData = async () => {
+  // await delay(1000);
+  const user = await getUserFromCookie(cookies());
+  const projects = await db.project.findMany({
+    where: {
+      ownerId: user?.id,
+    },
+    include: {
+      tasks: true,
+    },
+  });
+
+  return { projects };
+};
+
 export default async function Page() {
+
+  const {projects} = getData();
+  console.log('projects: ', projects);
+
   return (
     <div className="h-full overflow-y-auto pr-6 w-full">
       <div className=" h-full  items-stretch justify-center min-h-[content]">
         <div className="flex-1 grow flex">{/** greetings here */}</div>
         <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
-          {/** projects map here */}
+          {projects.map(project => (
+            <div>
+              <Link></Link>
+            </div>
+          ))}
           <Suspense fallback={<GreetingsSkeleton/>}><Greetings/></Suspense>
           <div className="w-1/3 p-3">{/* new project here */}</div>
         </div>
